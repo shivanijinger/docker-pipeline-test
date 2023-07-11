0e7a3eb44c5c8d31c62e0548dc7195c90e8193d2
shivanijinger <138754274+shivanijinger@users.noreply.github.com>
2023-07-11 17:11:02 +0530

    Created error in app.py

--- app.py
+++ app.py
@@ -2,18 +2,12 @@
 Main application file
 """
 from flask import Flask
-import logging
 app = Flask(__name__)
-
-# Initialize Logger
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    LOGGER.info('Received a message: %s', random_string)
-    return "".join(reversed(random_string))
+    return "Breaking the unit test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
